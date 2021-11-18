d78f2f112ed2ce68ed419e2d254fc7e925af73c2
Victor Abreu Azevedo <azevedovictor6699@gmail.com>
2021-11-17 23:23:57 -0300

    Parte 2 - Desenvolvendo Jquery com Bootstrap

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 function consultaCep(){
     let cep = $('#cep').val();
-    
+    $('.barra-progresso').show();
     $.ajax({
         url: `https://viacep.com.br/ws/${cep}/json/unicode/`,
         type: "GET",
         success: function (response) {
+            $('#cep-titulo').html("CEP: "+response.cep);
             $('#logradouro').html(response.logradouro);
             $('#bairro').html(response.bairro);
             $('#localidade').html(response.localidade);
-            $('#uf').html(response.uf);
+            $('#uf').html(response.uf);  
+            $('#table').show(); 
+            $('.barra-progresso').hide();         
         }
     });
 }
+
+jQuery(function($) {
+	$('#table').hide();
+    $('.barra-progresso').hide();         
+});
+
